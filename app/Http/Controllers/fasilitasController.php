b701ef7a07a3a3ac30dd1f4b0bc4751db7b5b425
MAfatturrahman <aliffaturrahman@gmail.com>
2022-11-14 10:06:42 +0700

    CRUD Fasilitas

---
+++ app/Http/Controllers/fasilitasController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\fasilitas;
+
+class fasilitasController extends Controller
+{
+    // Untuk memanggil halaman fasilitas(Halaman Admin)
+    public function index()
+    {
+        $fasilitas = fasilitas::all();
+        return view('Backend.fasilitas.index', compact(
+            'fasilitas'
+        ));
+    }
+
+    // Untuk memanggil halaman create(Halaman Admin)
+    public function create()
+    {
+        return view('Backend.fasilitas.create');
+    }
+
+    // Untuk menyimpan data yang di berikan dari petugas(Halaman Admin)
+    public function store(Request $request)
+    {
+        $fasilitas = new fasilitas;
+        $fasilitas->gambar_fasiliats = $request->gambar_fasilitas;
+        $fasilitas->judul_fasilitas = $request->judul_fasilitas;
+        $fasilitas->deskripsi_fasilitas = $request->deskripsi_fasilitas;
+        $fasilitas->save();
+        return redirect()->view('fasilitas.index')->with('alert', 'Data Fasilitas Berhasil di Tambah');
+    }
+
+    // Untuk memanggil halaman show(Admin Page)
+    public function show($id)
+    {
+        $fasiliats = fasilitas::find($id);
+        return view('Backend.fasilitas.show', compact(
+            'fasilitas'
+        ));
+    }
+
+    // Untuk memanggil halaman edit(Halaman Admin)
+    public function edit($id)
+    {
+        $fasilitas = fasilitas::find($id);
+        return view('Backend.fasilitas.edit', compact(
+            'fasilitas'
+        ));
+    }
+
+    // Untuk menyimpan data apa saja yang telah di ubah(Halaman Admin) 
+    public function update(Request $request, $id)
+    {
+        $fasilitas = fasilitas::find($id);
+        $fasilitas->gambar_fasilitas = $request->gambar_fasilitas;
+        $fasilitas->judul_fasilitas = $request->judul_fasilitas;
+        $fasilitas->deskripsi_fasilitas = $request->deskripsi_fasilitas;
+        $fasilitas->save();
+        return redirect()->view('fasilitas.index')->with('alert', 'Data Fasilitas Berhasil di Edit');
+    }
+
+    // Untuk menghapus data fasilitas(Halaman Admin)
+    public function destroy($id)
+    {
+        $fasilitas = fasilitas::find($id)->delete();
+        return redirect()->view('fasilitas.index')->with('alert', 'Data Fasilitas Berhasil di Hapus');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\petugasController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\petugasController;
+use App\Http\Controllers\makananController;
+use App\Http\Controllers\minumanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,4 +36,27 @@ Route::group(['middleware' => ['auth']], function () {
     Route::post('/petugas', [petugasController::class, 'store'])->name('petugas.store')->middleware();
     Route::put('/petugas/{$id}', [petugasController::class, 'update'])->name('petugas.update')->middleware();
     // ----------------------------------------------------------------------------------------------------
+
+    // Makanan
+    // ----------------------------------------------------------------------------------------------------
+    Route::get('/makanan', [makananController::class, 'index'])->name('makanan.index')->middleware();
+    Route::get('/makanan/create', [makananController::class, 'create'])->name('makanan.create')->middleware();
+    Route::get('/makanan/{$id}/edit', [makananController::class, 'edit'])->name('makanan.edit')->middleware();
+    Route::get('/makanan/{$id}', [makananController::class, 'show'])->name('makanan.show')->middleware();
+    Route::delete('/makanan/{$id}', [makananController::class, 'destroy'])->name('makanan.destroy')->middleware();
+    Route::post('/makanan', [makananController::class, 'store'])->name('makanan.store')->middleware();
+    Route::put('/makanan/{$id}', [makananController::class, 'update'])->name('makanan.update')->middleware();
+    // ----------------------------------------------------------------------------------------------------
+
+    // Minuman
+    // ----------------------------------------------------------------------------------------------------
+    Route::get('/minuman', [minumanController::class, 'index'])->name('minuman.index')->middleware();
+    Route::get('/minuman/create', [minumanController::class, 'create'])->name('minuman.create')->middleware();
+    Route::get('/minuman/{$id}/edit', [minumanController::class, 'edit'])->name('minuman.edit')->middleware();
+    Route::get('/minuman/{$id}', [minumanController::class, 'show'])->name('minuman.show')->middleware();
+    Route::delete('/minuman/{$id}', [minumanController::class, 'destroy'])->name('minuman.destroy')->middleware();
+    Route::post('/minuman', [minumanController::class, 'store'])->name('minuman.store')->middleware();
+    Route::put('/minuman{$id}', [minumanController::class, 'update'])->name('minuman.update')->middleware();
+    // ----------------------------------------------------------------------------------------------------
+
 });
