81ef7f786de290e4a47cb38c6f8c89fae7a157be
MAfatturrahman <aliffaturrahman@gmail.com>
2022-12-29 14:39:50 +0700

    Make user management responsif

--- resources/views/Backend/userManagement/create.blade.php
+++ resources/views/Backend/userManagement/create.blade.php
@@ -5,52 +5,64 @@
     <div class="d-flex flex-row justify-content-between">
         <p class="btn btn-primary mt-3">Halaman Tambah User</p>
     </div>
-    <div class="d-flex flex-row gap-4">
-        <div class="my_card w-75">
+    <div class="d-flex flex-sm-row flex-column gap-4">
+        <div class="my_card align-self-baseline">
             <form method="POST" action="{{ route('userManagement.store') }}">
-                <div class="form-group row">
+                <div class="form-group">
                     @csrf
-                    <label class="col-sm-3 col-form-label mb-3">Nama</label>
-                    <div class="col-sm-7">
-                        <input class="form-control" type="text" name="name" placeholder="Nama" required>
+                    <div class="mb-3 d-flex flex-sm-row flex-column align-items-start align-items-sm-center h-auto gap-2">
+                        <label class="form-label" style="width: 150px">Nama</label>
+                        <div class="w-100">
+                            <input class="form-control" type="text" name="name" placeholder="Nama" required>
+                        </div>
                     </div>
-                    <label class="col-sm-3 col-form-label mb-3">Email</label>
-                    <div class="col-sm-7">
-                        <input class="form-control" type="text" name="email" placeholder="pengguna@gmail.com" required>
+
+                    <div class="mb-3 d-flex flex-sm-row flex-column align-items-start align-items-sm-center h-auto gap-2">
+                        <label class="form-label" style="width: 150px">Email</label>
+                        <div class="w-100">
+                            <input class="form-control" type="text" name="email" placeholder="pengguna@gmail.com"
+                                required>
+                        </div>
                     </div>
-                    <label class="col-sm-3 col-form-label mb-3">Password</label>
-                    <div class="col-sm-7">
-                        <input class="form-control" type="text" name="password" placeholder="Pa$$w0rd" required>
+
+                    <div class="mb-3 d-flex flex-sm-row flex-column align-items-start align-items-sm-center h-auto gap-2">
+                        <label class="form-label" style="width: 150px">Password</label>
+                        <div class="w-100">
+                            <input class="form-control" type="text" name="password" placeholder="Pa$$w0rd" required>
+                        </div>
                     </div>
-                    <label class="col-sm-3 col-form-label mb-5">Role</label>
-                    <div class="col-sm-3">
-                        <x-adminlte-select2 name="role" label-class="text-lightblue" igroup-size="sm"
-                            data-placeholder="Pilih Role ...">
-                            <x-slot name="prependSlot">
-                                <div class="input-group-text bg-gradient-info">
-                                    <i class="fas fa-fw fa-lock"></i>
-                                </div>
-                            </x-slot>
-                            </option>
-                            <option selected disabled>Pilih Role</option>
-                            @foreach ($roles as $role)
-                                @if ($role->id == 1)
-                                    <option disabled value="{{ $role->name }}">{{ $role->name }}</option>
-                                @else
-                                    <option value="{{ $role->name }}">{{ $role->name }}</option>
-                                @endif
-                            @endforeach
-                        </x-adminlte-select2>
+
+                    <div class="mb-3 d-flex flex-sm-row flex-column align-items-start align-items-sm-center h-auto gap-2">
+                        <label class="form-label" style="width: 150px">Role</label>
+                        <div class="w-100">
+                            <x-adminlte-select2 name="role" label-class="text-lightblue" igroup-size="sm"
+                                data-placeholder="Pilih Role ...">
+                                <x-slot name="prependSlot">
+                                    <div class="input-group-text bg-gradient-info">
+                                        <i class="fas fa-fw fa-lock"></i>
+                                    </div>
+                                </x-slot>
+                                </option>
+                                <option selected disabled>Pilih Role</option>
+                                @foreach ($roles as $role)
+                                    @if ($role->id == 1)
+                                        <option disabled value="{{ $role->name }}">{{ $role->name }}</option>
+                                    @else
+                                        <option value="{{ $role->name }}">{{ $role->name }}</option>
+                                    @endif
+                                @endforeach
+                            </x-adminlte-select2>
+                        </div>
                     </div>
                 </div>
-                <div class="d-flex flex-row justify-content-between">
+                <div class="d-flex flex-sm-row flex-column gap-2 justify-content-between">
                     <a class="btn btn-primary" href="{{ url('userManagement') }}">Kembali</a>
                     <button class="btn btn-success" type="submit">Tambahkan</button>
                 </div>
             </form>
         </div>
 
-        <div class="my_card w-50">
+        <div class="my_card align-self-baseline">
             <p class="text-center" style="color: red; font-size: 24px;">Perhatian</p>
             <p style="line-height: 1.8">Tata cara pengisian untuk menambahkan user baru :
                 <br> 1. Nama Harus di isi, nama tersebut akan menjadi nama panggilan
--- resources/views/Backend/userManagement/show.blade.php
+++ resources/views/Backend/userManagement/show.blade.php
@@ -5,37 +5,48 @@
     <div class="d-flex flex-row justify-content-between">
         <p class="btn btn-primary mt-3">Halaman Show User</p>
     </div>
-    <div class="d-flex flex-row gap-4">
-        <div class="my_card w-75">
-            <div class="form-group row">
-                @csrf
-                <label class="col-sm-3 col-form-label mb-3">Nama</label>
-                <div class="col-sm-7">
-                    <input class="form-control" type="text" name="name" placeholder="Nama" value="{{ $user->name }}"
-                        required onclick="false">
+
+    <div class="d-flex flex-sm-row flex-column gap-4">
+        <div class="my_card align-self-baseline">
+            <div class="form-group">
+                <div class="mb-3 d-flex flex-sm-row flex-column align-items-start align-items-sm-center h-auto gap-2">
+                    <label class="form-label" style="width: 150px">Nama</label>
+                    <div class="w-100">
+                        <input class="form-control" type="text" name="name" placeholder="Nama"
+                            value="{{ $user->name }}" required onclick="false">
+                    </div>
                 </div>
-                <label class="col-sm-3 col-form-label mb-3">Email</label>
-                <div class="col-sm-7">
-                    <input class="form-control" type="text" name="email" placeholder="pengguna@gmail.com"
-                        value="{{ $user->email }}" required onclick="false">
+
+                <div class="mb-3 d-flex flex-sm-row flex-column align-items-start align-items-sm-center h-auto gap-2">
+                    <label class="form-label" style="width: 150px">Email</label>
+                    <div class="w-100">
+                        <input class="form-control" type="text" name="email" placeholder="pengguna@gmail.com"
+                            value="{{ $user->email }}" required onclick="false">
+                    </div>
                 </div>
-                <label class="col-sm-3 col-form-label mb-3">Role</label>
-                <div class="col-sm-7">
-                    <input class="form-control" type="text" name="role" placeholder="Role"
-                        value="{{ $user->getRoleNames()[0] }}" required onclick="false">
+
+                <div class="mb-3 d-flex flex-sm-row flex-column align-items-start align-items-sm-center h-auto gap-2">
+                    <label class="form-label" style="width: 150px">Role</label>
+                    <div class="w-100">
+                        <input class="form-control" type="text" name="role" placeholder="Role"
+                            value="{{ $user->getRoleNames()[0] }}" required onclick="false">
+                    </div>
                 </div>
             </div>
 
-            <label class="col-sm-3 col-form-label mb-3" hidden onclick="false">Password</label>
-            <div class="col-sm-7">
-                <input class="form-control" type="text" name="password" placeholder="Pa$$w0rd"
-                    value="{{ $user->password }}" required hidden>
+            <div class="mb-3 d-flex flex-sm-row flex-column align-items-start align-items-sm-center h-auto gap-2">
+                <label class="form-label" style="width: 150px" hidden onclick="false">Password</label>
+                <div class="w-100">
+                    <input class="form-control" type="text" name="password" placeholder="Pa$$w0rd"
+                        value="{{ $user->password }}" required hidden>
+                </div>
             </div>
-            <div class="d-flex flex-row justify-content-between">
+
+            <div class="d-flex flex-sm-row flex-column justify-content-between">
                 <a class="btn btn-primary" href="{{ url('userManagement') }}">Kembali</a>
             </div>
         </div>
-        <div class="my_card w-50">
+        <div class="my_card align-self-baseline">
             <p class="text-center" style="color: red; font-size: 24px;">Perhatian</p>
             <p style="line-height: 1.8">Tata cara pengisian untuk menambahkan user baru :
                 <br> 1. Nama Harus di isi, nama tersebut akan menjadi nama panggilan
